c2194b9b38fbb6f3ba7257accf749f773c8e96b9
Jill Allan <jillallan24@gmail.com>
2022-11-09 08:46:59 +0000

    Update content view

--- Trip Journal/ContentView.swift
+++ Trip Journal/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             List {
-                NavigationLink("Map View") {
+                NavigationLink("TripView") {
                     TripView()
                 }
                 NavigationLink("") {
