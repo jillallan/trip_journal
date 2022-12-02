544d85e3a50a61904730b16112fa171673c9d9bb
Jill Allan <jillallan24@gmail.com>
2022-12-02 19:10:34 +0000

    Add onChange to Binding

---
+++ Trip Journal/Extensions/Binding-OnChange.swift
@@ -0,0 +1,20 @@
+//
+//  Binding-OnChange.swift
+//  Trip Journal
+//
+//  Created by Jill Allan on 01/12/2022.
+//
+
+import SwiftUI
+
+extension Binding {
+    func onChange(_ handler: @escaping () -> Void) -> Binding<Value> {
+        Binding(
+            get: { self.wrappedValue },
+            set: { newValue in
+                self.wrappedValue = newValue
+                handler()
+            }
+        )
+    }
+}
